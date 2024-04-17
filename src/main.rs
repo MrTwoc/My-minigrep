94835069335de28ceeacc3435fb445b99a55514e
TwoC-Mr <1191422391@qq.com>
2024-04-17 15:19:18 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::env;
+
+fn main() {
+    let arge :Vec<String> = env::args().collect();
+
+    let query = &arge[1];
+    let file_name = &arge[2];
+    println!("查找内容：{}", query);
+    println!("文件名：{}", file_name);
+}
