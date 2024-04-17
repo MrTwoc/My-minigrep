14be0f84c5fe4dfec790997898d87ae5b69cf175
TwoC-Mr <1191422391@qq.com>
2024-04-17 16:53:56 +0800

    新增-text.txt: 示例文件 并在main.rs中读取并输出

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let arge :Vec<String> = env::args().collect();
-
+    
+    // query, file_name :  查找内容, 文件名
     let query = &arge[1];
     let file_name = &arge[2];
     
-    println!("查找内容：{}", query);
-    println!("文件名：{}", file_name);
+    // println!("查找内容：{}", query);
+    // println!("文件名：{}", file_name);
+
+    let contents = fs::read_to_string(file_name)
+    .expect("读取文件失败");
+
+    println!("文件内容：\n{}", contents);
 }
