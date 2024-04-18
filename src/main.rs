ac2ddbb35bb7216147ecdcc8f1ad17761a4b811a
TwoC-Mr <1191422391@qq.com>
2024-04-18 17:02:02 +0800

    重构代码模块

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,23 @@ use std::fs;
 
 fn main() {
     // 接收输入的参数
-    let arge :Vec<String> = env::args().collect();
-    
-    // query, file_name :  查找内容, 文件名
-    let query = &arge[1];
-    let file_name = &arge[2];
-    
-    // println!("查找内容：{}", query);
-    // println!("文件名：{}", file_name);
-
-    let contents = fs::read_to_string(file_name)
-    .expect("读取文件失败");
-
+    let args :Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+    let contents = fs::read_to_string(config.file_name).expect("读取文件失败");
     println!("文件内容：\n{}", contents);
+}
+
+struct Config{
+    query: String,
+    file_name: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config{
+        // query, file_name :  查找内容, 文件名
+        let query = args[1].clone();
+        let file_name = args[2].clone();
+    
+        Config{query, file_name}
+    }
 }
