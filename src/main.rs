24e6ccc39cd49012801fa7082833b4e2b3c4f9d4
TwoC-Mr <1191422391@qq.com>
2024-04-19 22:03:58 +0800

    将错误信息输出到标准错误
    println() -> eprintln()
    P68

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     // 接收输入的参数
     let args :Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("错误：{}", err);
+        eprintln!("错误：{}", err);
         process::exit(1);
     });
     
     if let Err(e)= minigrep::run(config){
-        println!("应用错误：{}", e);
+        eprintln!("应用错误：{}", e);
         process::exit(1);
     }
     
